5950e54c9bb4cd8cff3980b1089703d3bfc30fd8
Joe Johnston <104495+simple10@users.noreply.github.com>
2024-06-15 14:27:43 -0700

    1.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,31 +2,27 @@
 file.
 
 ####
-[v1.0.4](https://github.com/levelupghl/ghlchattools/compare/v1.0.3...v1.0.4)
+[v1.0.5](https://github.com/levelupghl/ghlchattools/compare/v1.0.4...v1.0.5)
 
 -
-Fix minified build
-[`d486858`](https://github.com/levelupghl/ghlchattools/commit/d486858450dc150241d91254cac8191ffbefc538)
+Add support for mobile browser & autoScroll
+[`2c7992d`](https://github.com/levelupghl/ghlchattools/commit/2c7992d90b5a6087743ea3268b103c04029adb63)
+-
+Update embedChat.ts
+[`8c7a5c0`](https://github.com/levelupghl/ghlchattools/commit/8c7a5c0e16b80308d8af370bf357ba7f63ec2e42)
+-
+Update README.md
+[`1a526e7`](https://github.com/levelupghl/ghlchattools/commit/1a526e7dcfae1450c7ea50512bac686c22397676)
 
 ####
-[v1.0.3](https://github.com/levelupghl/ghlchattools/compare/v1.0.2...v1.0.3)
+v1.0.4
 
 >
 31 May 2024
 
 -
-Change build from iife to es module
-[`27a52c7`](https://github.com/levelupghl/ghlchattools/commit/27a52c77017db8cf048abba207e460750b7ae288)
--
-Change default selector to #ghl-chat-embed
-[`4ef462e`](https://github.com/levelupghl/ghlchattools/commit/4ef462edd04774e2f5c6b0b33433403408c4e60e)
-
-####
-[v1.0.2](https://github.com/levelupghl/ghlchattools/compare/v1.0.1...v1.0.2)
-
->
-31 May 2024
-
+Init
+[`ae29453`](https://github.com/levelupghl/ghlchattools/commit/ae29453362e6d89cd2e3fa474c8b904cfa5b4322)
 -
 Rebuild
 [`9682a62`](https://github.com/levelupghl/ghlchattools/commit/9682a62230204c3a9389070614834405bc4930e6)
@@ -34,15 +30,14 @@ Rebuild
 Update to use GHL chat widget API
 [`2c9e9d0`](https://github.com/levelupghl/ghlchattools/commit/2c9e9d0c42a009030fc2c9dd96ed62cf152c6e02)
 -
+Change build from iife to es module
+[`27a52c7`](https://github.com/levelupghl/ghlchattools/commit/27a52c77017db8cf048abba207e460750b7ae288)
+-
+Change default selector to #ghl-chat-embed
+[`4ef462e`](https://github.com/levelupghl/ghlchattools/commit/4ef462edd04774e2f5c6b0b33433403408c4e60e)
+-
+Fix minified build
+[`d486858`](https://github.com/levelupghl/ghlchattools/commit/d486858450dc150241d91254cac8191ffbefc538)
+-
 Update tsconfig.json
 [`3c627d9`](https://github.com/levelupghl/ghlchattools/commit/3c627d9187bf5af83c8e8adbd0af1c2cdbe62549)
-
-####
-v1.0.1
-
->
-27 May 2024
-
--
-Init
-[`ae29453`](https://github.com/levelupghl/ghlchattools/commit/ae29453362e6d89cd2e3fa474c8b904cfa5b4322)
--- dist/js/embedChat.js
+++ dist/js/embedChat.js
@@ -1,9 +1,53 @@
 /*!***************************************
  * Level Up GHL Chat Tools
  * https//levelupghl.com
- * Version: v1.0.4
+ * Version: v1.0.5
  ****************************************/
 
+function isMobileBrowser() {
+  const userAgent = navigator.userAgent || navigator.vendor;
+  const mobileAgents = [
+    /android/i,
+    /iphone/i,
+    /ipad/i,
+    /ipod/i,
+    /blackberry/i,
+    /windows phone/i,
+    /opera mini/i,
+    /iemobile/i,
+    /mobile/i
+  ];
+  return mobileAgents.some((mobileAgent) => mobileAgent.test(userAgent));
+}
+function browserHeightLessThan(maxHeight) {
+  const currentHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  return currentHeight < maxHeight;
+}
+function scrollToElement(elem, scrollOffset = 0) {
+  const elementPosition = elem.getBoundingClientRect().top + window.pageYOffset;
+  const offsetPosition = elementPosition - scrollOffset;
+  window.scrollTo({
+    top: offsetPosition,
+    behavior: "smooth"
+  });
+}
+
+var __defProp = Object.defineProperty;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b || (b = {}))
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -26,6 +70,11 @@ var __async = (__this, __arguments, generator) => {
 };
 const CHAT_EMBED_CONTAINER_SELECTOR = "#ghl-chat-embed";
 const GHL_CHAT_WIDGET_SELECTOR = "chat-widget";
+const DEFAULT_OPTIONS = {
+  maxHeight: 650,
+  autoScroll: true,
+  scrollOffset: 20
+};
 function waitForChatLoad() {
   return __async(this, null, function* () {
     return new Promise((resolve) => {
@@ -37,9 +86,30 @@ function waitForChatLoad() {
     });
   });
 }
-const embedChat = (containerSelector) => __async(void 0, null, function* () {
+function getOptions(options) {
+  const opts = __spreadValues({}, DEFAULT_OPTIONS);
+  if (!options) {
+    return opts;
+  }
+  try {
+    if (options.maxHeight) {
+      opts.maxHeight = typeof options.maxHeight === "number" ? options.maxHeight : parseInt(options.maxHeight);
+    }
+    if (options.autoScroll) {
+      opts.autoScroll = true;
+      if (options.scrollOffset) {
+        opts.scrollOffset = typeof options.scrollOffset === "number" ? options.scrollOffset : parseInt(options.scrollOffset);
+      }
+    }
+  } catch (err) {
+    console.error(`embedChat: invalid options, using defaults`);
+  }
+  return opts;
+}
+const embedChat = (containerSelector, options) => __async(void 0, null, function* () {
   var _a, _b, _c;
   const selector = containerSelector || CHAT_EMBED_CONTAINER_SELECTOR;
+  const opts = getOptions(options);
   const embedDiv = document.querySelector(selector);
   if (!embedDiv) {
     console.error(
@@ -50,12 +120,13 @@ const embedChat = (containerSelector) => __async(void 0, null, function* () {
   if (!((_b = (_a = window.leadConnector) == null ? void 0 : _a.chatWidget) == null ? void 0 : _b.isLoaded)) {
     yield waitForChatLoad();
   }
+  if (isMobileBrowser() || browserHeightLessThan(opts.maxHeight)) {
+    return window.leadConnector.chatWidget.openWidget();
+  }
   const chat = document.querySelector(GHL_CHAT_WIDGET_SELECTOR);
   const root = chat == null ? void 0 : chat.shadowRoot;
   if (!chat || !root) {
-    console.error(
-      `embedChat: unable to find GHL chat widget on page`
-    );
+    console.error(`embedChat: unable to find GHL chat widget on page`);
     return;
   }
   const button = root.querySelector(".lc_text-widget--btn");
@@ -65,7 +136,9 @@ const embedChat = (containerSelector) => __async(void 0, null, function* () {
     ".lc_text-widget_heading--root"
   );
   if (!button || !widget || !box || !heading) {
-    console.error(`embedChat: unable to embed GHL chat widget in page: widget components not found`);
+    console.error(
+      `embedChat: unable to embed GHL chat widget in page: widget components not found`
+    );
     return;
   }
   (_c = chat.parentElement) == null ? void 0 : _c.removeChild(chat);
@@ -75,6 +148,11 @@ const embedChat = (containerSelector) => __async(void 0, null, function* () {
   button.style.display = "none";
   embedDiv.appendChild(chat);
   window.leadConnector.chatWidget.openWidget();
+  if (opts.autoScroll) {
+    setTimeout(() => {
+      scrollToElement(embedDiv, opts.scrollOffset);
+    }, 100);
+  }
 });
 
 export { embedChat };
